3d6ef4746702feee8a58d3652347b1c685ea07d9
Doni Pataki <yellow.pataki.doni@gmail.com>
2018-04-16 11:08:30 +0200

    Fix restAPI with mongoDB

---
+++ blog-backEnd/server.js
@@ -0,0 +1,57 @@
+const express = require('express')
+const path = require('path')
+const bodyParser = require('body-parser')
+const mongoose = require('mongoose')
+const db = require('./config/database')
+const helmet = require('helmet')
+const morgan = require('morgan')
+const fs = require('fs')
+const https = require('https');
+
+const postRouter = require('./routes/post.route')
+const app = express()
+
+
+// Connect to MongoDB
+mongoose.connect(db.uri, db.options).then(
+  () => {
+    console.log('MongoDB connected.')
+  },
+  err => {
+    console.error('MongoDB error.:' + err)
+  }
+)
+
+// Get view File Path
+/*function view (path) {
+  return __dirname + '/views/' + path
+}*/
+
+// Parse application/x-www-form-urlencoded
+app.use(bodyParser.urlencoded({
+  extended: false
+}))
+
+// Parse application/json
+app.use(bodyParser.json())
+
+// Minden kérés loggolása
+app.use(morgan('dev', {stream: fs.createWriteStream('./access.log', {flags: 'a'})}))
+
+// basic secure
+app.use(helmet())
+
+
+
+// Home page
+app.use('/post', postRouter)
+
+
+
+// Start server
+// const server = https.createServer(httpsOptions, app)
+// server.listen(3003, function(){
+//   console.log("server running at https://localhost:3000/")
+// });
+
+app.listen(3004);
